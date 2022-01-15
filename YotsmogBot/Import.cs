fb29bdf1685599c4ea5bff127e4c91fede309319
AHpx <AHpx@yandex.com>
2022-01-15 22:01:45 +0800

    Added global importer

---
+++ YotsmogBot/Import.cs
@@ -0,0 +1,5 @@
+﻿global using YotsmogBot.Utils;
+global using Mirai.Net.Data.Messages;
+global using Mirai.Net.Data.Messages.Receivers;
+global using Mirai.Net.Modules;
+global using Mirai.Net.Utils.Scaffolds;
