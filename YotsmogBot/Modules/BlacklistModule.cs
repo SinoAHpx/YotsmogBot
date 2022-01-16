549e27b0bb2c17f81854d6895143ccf31f593664
AHpx <AHpx@yandex.com>
2022-01-16 23:40:57 +0800

    Added simple BlacklistModule.cs

---
+++ YotsmogBot/Modules/BlacklistModule.cs
@@ -0,0 +1,98 @@
+﻿using AHpx.Extensions.StringExtensions;
+using Mirai.Net.Data.Commands;
+using Mirai.Net.Data.Messages.Concretes;
+
+namespace YotsmogBot.Modules;
+
+public class BlacklistModule : IModule
+{
+    public async void Execute(MessageReceiverBase @base)
+    {
+        var receiver = @base.Concretize<GroupMessageReceiver>();
+
+        try
+        {
+            //todo: bot administrator needed
+            if (receiver.Sender.Id == "2933170747" && receiver.MessageChain.GetPlainMessage().IsNotNullOrEmpty())
+            {
+                var plain = receiver.MessageChain.GetPlainMessage().Trim();
+
+                if (receiver.MessageChain.OfType<AtMessage>().Any())
+                    plain += receiver.MessageChain.OfType<AtMessage>().First().Target;
+
+
+                if (plain.StartsWith("/block"))
+                {
+                    var block = plain.ParseCommand<BlackList>();
+                    if (block.Group.IsNotNullOrEmpty())
+                    {
+                        await ConfigUtils.AddBlackListAsync(block.Group, true);
+
+                        if (receiver.Id == block.Group)
+                            await receiver.SendMessageAsync("本群已被屏蔽!");
+                        else
+                            await receiver.SendMessageAsync($"群{block.Group}已被屏蔽!");
+                    }
+                    if (block.User.IsNotNullOrEmpty())
+                    {
+                        await ConfigUtils.AddBlackListAsync(block.User, false);
+
+                        if (receiver.Sender.Id == block.User)
+                            await receiver.SendMessageAsync("你已被屏蔽!");
+                        else
+                            await receiver.SendMessageAsync($"{block.User}已被屏蔽!");
+                    }
+                }
+                
+                if (plain.StartsWith("/unblock"))
+                {
+                    var unblock = plain.ParseCommand<UnblockList>();
+                    if (unblock.Group.IsNotNullOrEmpty())
+                    {
+                        await ConfigUtils.RemoveBlackListAsync(unblock.Group);
+
+                        if (receiver.Id == unblock.Group)
+                            await receiver.SendMessageAsync("本群已被解除屏蔽!");
+                        else
+                            await receiver.SendMessageAsync($"群{unblock.Group}已被解除屏蔽!");
+                    }
+                    if (unblock.User.IsNotNullOrEmpty())
+                    {
+                        await ConfigUtils.RemoveBlackListAsync(unblock.User);
+
+                        if (receiver.Sender.Id == unblock.User)
+                            await receiver.SendMessageAsync("你已被解除屏蔽!");
+                        else
+                            await receiver.SendMessageAsync($"{unblock.User}已被解除屏蔽!");
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            new Logger().Log(e);
+        }
+    }
+
+    public bool? IsEnable { get; set; }
+
+    [CommandEntity(Name = "block")]
+    class BlackList
+    {
+        [CommandArgument(Name = "group")]
+        public string Group { get; set; }
+
+        [CommandArgument(Name = "user")]
+        public string User { get; set; }
+    }
+    
+    [CommandEntity(Name = "unblock")]
+    class UnblockList
+    {
+        [CommandArgument(Name = "group")]
+        public string Group { get; set; }
+
+        [CommandArgument(Name = "user")]
+        public string User { get; set; }
+    }
+}
--- YotsmogBot/Utils/ILogger.cs
+++ YotsmogBot/Utils/ILogger.cs
@@ -14,6 +14,7 @@ public class Logger : ILogger
     public void Log(string message)
     {
         AnsiConsole.MarkupLine($@"[red]{DateTime.Now:s}[/] - {message}");
+        AnsiConsole.Markup("[red]YotsmogBot[/]> ");
     }
 
     public void Log(Exception exception)
