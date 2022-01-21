e04004864f67a30aa89d902788c8431a717196b3
AHpx <AHpx@yandex.com>
2022-01-21 22:59:55 +0800

    Added ParrotModule.cs

--- YotsmogBot.Test/Program.cs
+++ YotsmogBot.Test/Program.cs
@@ -1,3 +1,7 @@
-﻿using YotsmogBot.Utils;
+﻿using AHpx.Extensions.StringExtensions;
+using Mirai.Net.Data.Messages.Concretes;
 
-ConfigUtils.GetApiKeyAsync("");
+var m1 = new PlainMessage("awdawd").ToJsonString();
+var m2 = new PlainMessage("awdawd").ToJsonString();
+
+Console.WriteLine(m1 == m2);
---
+++ YotsmogBot/Modules/ParrotModule.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using AHpx.Extensions.StringExtensions;
+using Mirai.Net.Data.Messages.Concretes;
+
+namespace YotsmogBot.Modules;
+
+[Description("复读机模块，复读机模块，复读机模块")]
+public class ParrotModule : IModule
+{
+    private static readonly List<IEnumerable<MessageBase>> ParrotList = new();
+
+    public async void Execute(MessageReceiverBase @base)
+    {
+        var receiver = @base.Concretize<GroupMessageReceiver>();
+
+        try
+        {
+            var messages = receiver.MessageChain.ToList();
+            messages.RemoveAt(0);
+
+            if (!ParrotList.Any())
+                ParrotList.Add(messages);
+
+            if (!ParrotList.Any())
+                ParrotList.Add(messages);
+
+            var messageJson = messages.Select(MapMessage);
+            var parrotList = ParrotList.Select(x => x.Select(MapMessage));
+            
+            if (parrotList.All(x => x.SequenceEqual(messageJson)))
+                ParrotList.Add(messages);
+            else
+                ParrotList.Clear();
+
+            if (ParrotList.Count > 3)
+            {
+                await receiver.SendMessageAsync(ParrotList.First().ToArray());
+                ParrotList.Clear();
+            }
+        }
+        catch (Exception e)
+        {
+            LoggerManager.Log(e);
+        }
+    }
+
+    public string MapMessage(MessageBase @base)
+    {
+        switch (@base.Type)
+        {
+            case Messages.Image: return (@base as ImageMessage)!.ImageId;
+            case Messages.Plain: return (@base as PlainMessage)!.Text;
+            default:
+                return @base.ToJsonString();
+        }
+    }
+
+    public bool? IsEnable { get; set; }
+}
